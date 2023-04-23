adc211d79be2315d4e0adc80133068a78ac10d65
Saabitra Bera <63924604+Saabitra@users.noreply.github.com>
2023-04-23 13:43:28 +0000

    panels

--- saabi-weather/app/location/[city]/[lat]/[long]/page.tsx
+++ saabi-weather/app/location/[city]/[lat]/[long]/page.tsx
@@ -1,4 +1,6 @@
 import { getClient } from "@/apollo-client";
+import CalloutCard from "@/components/CalloutCard";
+import StatCard from "@/components/StatCard";
 import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
 
 type Props = {
@@ -18,7 +20,7 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
       current_weather: "true",
       latitude: lat,
       longitude: long,
-      timezone: 'GMT',
+      timezone: "GMT",
     },
   });
 
@@ -28,7 +30,32 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
 
   return (
     <div>
-      Welcome to the weather page: {city} {lat} {long}
+      {/* Info Panel */}
+
+      <div>
+        <div className="p-5">
+          <div className="pb-5">
+            <h2 className="text-xl font-bold">Todays Overview</h2>
+            <p className="text-sm text-gray-400">
+              Last Updated at:{" "}
+              {new Date(results.current_weather.time).toLocaleString()} (
+              {results.timezone})
+            </p>
+          </div>
+
+          <div>
+            <CalloutCard message="This is where GPT summary will go!" />
+          </div>
+
+          <div>
+            <StatCard 
+              title="Maximum Temperature"
+              metric={`${results.daily.temperature_2m_max[0].toFixed(1)}°C`}
+              color = "yellow"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
